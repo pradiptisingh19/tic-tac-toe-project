e69a510b7bbccc23e9ee80e80ea7a2fa8e8c745b
pradiptisingh19 <pradipti.singh8@gmail.com>
2024-06-25 16:38:04 +0530

    adding some logic in js

---
+++ logic.js
@@ -0,0 +1,39 @@
+let boxes=document.querySelectorAll(".box");
+let resetbtn=document.querySelector("#reset");
+let turn0=true;
+const winPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+function checkWinner(){
+    for(let pattern of winPatterns){
+        let pos1=boxes[pattern[0]].innerText;
+        let pos2=boxes[pattern[1]].innerText;
+        let pos3=boxes[pattern[2]].innerText;
+        if(pos1!=""&&pos2!=""&&pos3!=""){
+            if(pos1===pos2&&pos2===pos3){
+                console.log("winner");
+            }
+        }
+    }
+};
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(turn0){
+            box.innerText="0";
+            turn0=false;
+        }
+        else{
+            box.innerText="X";
+            turn0=true;
+        }
+        box.disabled=true;
+    });
+    checkWinner();
+});
